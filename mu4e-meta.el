465574bf5e8ea8777ea5d8e7e0f5749aed4e2736
Takaaki ISHIKAWA <takaxp@ieee.org>
2021-08-07 04:24:33 +0900

    Initial commit

---
+++ mu4e-meta.el
@@ -0,0 +1,11 @@
+;; auto-generated
+(defconst mu4e-mu-version "1.6.1"
+  "Required mu binary version; mu4e's version must agree with this.")
+
+(defconst mu4e-builddir "/private/tmp/mu-20210802-76006-kiflhg/mu-1.6.1"
+  "Top-level build directory.")
+
+(defconst mu4e-doc-dir "/usr/local/Cellar/mu/1.6.1/share/doc/mu"
+  "Mu4e's data-dir.")
+
+(provide 'mu4e-meta)
